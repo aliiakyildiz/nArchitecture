f63974bea3be9d14e1dbbdb78a476c37169c0993
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-08-16 00:54:47 +0300

    Created Layers

--- src/demoProjects/templateProject/WebAPI/Program.cs
+++ src/demoProjects/templateProject/WebAPI/Program.cs
@@ -17,8 +17,6 @@ builder.Services.AddInfrastructureServices();
 builder.Services.AddHttpContextAccessor();
 
 
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
